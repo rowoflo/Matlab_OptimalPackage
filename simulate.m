de221821deda65b0389689701162b15a3e4dc0df
Rowland O'Flaherty <rowoflo@gmail.com>
2015-01-29 14:52:21 -0500

    Updated the simulate function simulate forward and backward with a policy function that outputs an input.

--- simulate.m
+++ simulate.m
@@ -1,26 +1,40 @@
-function [x,xD] = simulate(f,t,x0,xm,xM,varargin)
+function [x,u,xDot] = simulate(f,g,t,x0,xm,xM,varargin)
 % The "simulate" function integrates the state in time.
 %
 % SYNTAX:
-%   [t,x,xD] = optimal.simulate(f,x0,t)
-%   [t,x,xD] = optimal.simulate(f,x0,t,'PropertyName',PropertyValue,...)
+%   [x,u,xDot] = optimal.simulate(f,g,x0,t)
+%   [x,u,xDot] = optimal.simulate(f,g,x0,t,xm,xM)
+%   [x,u,xDot] = optimal.simulate(f,g,x0,t,xm,xM,'PropertyName',PropertyValue,...)
 % 
 % INPUTS:
 %   f - (function_handle)
 %       State dynamics.
 %       SYNTAX:
-%           xDot = f(x,t);
+%           xDot = f(x,u,t,k);
 %       INPUTS:
-%           x - (n x tn number) State.
-%           t - (1 x tn number) Time.
+%           x - (n x 1 number) State.
+%           u - (m x 1 number) Input.
+%           t - (1 x 1 number) Time.
+%           k - (1 x 1 integer) Step number (Matlab indexing).
 %       OUTPUTS:
 %           xDot - (n x tn number) State derivative.
+%
+%   g - (function_handle)
+%       Policy.
+%       SYNTAX:
+%           u = g(x,t,k);
+%       INPUTS:
+%           x - (n x 1 number) State.
+%           t - (1 x 1 number) Time.
+%           k - (1 x 1 integer) Step number (Matlab indexing).
+%       OUTPUTS:
+%           u - (m x 1 number) Input.
 %
 %   t - (1 x tn number)
 %       Time trajectory.
 %
 %   x0 - (n x 1 number) 
-%       Initial conidtion.
+%       Initial state.
 %
 %   xm - (n x 1 number)
 %       Minimum state constraint.
@@ -30,14 +44,18 @@ function [x,xD] = simulate(f,t,x0,xm,xM,varargin)
 %   
 %
 % PROPERTIES: TODO: Add properties
-%   'propertiesName' - (size type) [defaultPropertyValue]
-%       Description.
+%   'direction' - ('forward' or 'backward') ['forward']
+%       Direction of simulation. If 'forward' then "x0" is the state at
+%       time "t[0]", 'backward' then "x0" is the state at time "t[tn]".
 % 
 % OUTPUTS:
 %   x - (n x tn number) 
 %       Solution trajectory.
 %
-%   xD - (n x tn number) 
+%   u - (m x tn-1 number)
+%       Input trajectory
+%
+%   xDot - (n x tn number) 
 %       Solution time derivative trajectory.
 %
 % EXAMPLES: TODO: Add examples
@@ -61,12 +79,17 @@ function [x,xD] = simulate(f,t,x0,xm,xM,varargin)
 %% Check Inputs
 
 % Check number of inputs
-narginchk(3,inf)
+narginchk(4,inf)
 
 % Check input arguments for errors
 assert(isa(f,'function_handle'),...
     'optimal:simulate:f',...
     'Input argument "f" must be a function handle.')
+
+assert(isa(g,'function_handle'),...
+    'optimal:simulate:g',...
+    'Input argument "g" must be a function handle.')
+m = size(g(0,0,1),1);
 
 assert(isnumeric(t) && isreal(t) && isvector(t),...
     'optimal:simulate:t',...
@@ -91,47 +114,72 @@ assert(isnumeric(xM) && isreal(xM) && isvector(xM) && length(xM) == n && all(xM 
     'Input argument "xM" must be a %d element vector of real numbers all greater or equal to xm.',n)
 
 %% Get and check properties
-% propargin = size(varargin,2);
-% 
-% assert(mod(propargin,2) == 0,...
-%     'optimal:simulate:properties',...
-%     'Properties must come in pairs of a "PropertyName" and a "PropertyValue".')
-%
-% propStrs = varargin(1:2:propargin);
-% propValues = varargin(2:2:propargin);
-% 
-% for iParam = 1:propargin/2
-%     switch lower(propStrs{iParam})
-%         case lower('propertyName')
-%             propertyName = propValues{iParam};
-%         otherwise
-%             error('optimal:simulate:options',...
-%               'Option string ''%s'' is not recognized.',propStrs{iParam})
-%     end
-% end
-% 
-% % Set to default value if necessary TODO: Add property defaults
-% if ~exist('propertyName','var'), propertyName = defaultPropertyValue; end
-% 
-% % Check property values for errors TODO: Add property error checks
-% assert(isnumeric(propertyName) && isreal(propertyName) && isequal(size(propertyName),[1,1]),...
-%     'optimal:simulate:propertyName',...
-%     'Property "propertyName" must be a ? x ? matrix of real numbers.')
+propargin = size(varargin,2);
 
-%% Initialize
-x = nan(n,tn);
-xD = nan(n,tn-1);
-x(:,1) = x0;
+assert(mod(propargin,2) == 0,...
+    'optimal:simulate:properties',...
+    'Properties must come in pairs of a "PropertyName" and a "PropertyValue".')
 
-%% Simulate forward
-for k = 1:tn-1
-    xD(:,k) = f(x(:,k),t(k));
-    ts = t(k+1) - t(k);
-    x(:,k+1) = constrain(x(:,k) + xD(:,k)*ts,xm,xM);
+propStrs = varargin(1:2:propargin);
+propValues = varargin(2:2:propargin);
+
+for iParam = 1:propargin/2
+    switch lower(propStrs{iParam})
+        case lower('direction')
+            direction = propValues{iParam};
+        otherwise
+            error('optimal:simulate:options',...
+              'Option string ''%s'' is not recognized.',propStrs{iParam})
+    end
+end
+
+% Set to default value if necessary
+if ~exist('direction','var'), direction = 'forward'; end
+
+% Check property values for errors TODO: Add property error checks
+assert(ischar(direction) && ismember(direction,{'forward','backward'}),...
+    'optimal:simulate:direction',...
+    'Property "direction" must be either ''forward'' or ''backward''.')
+
+%% Run
+if strcmp(direction,'forward') % Simulate forward
+    x = nan(n,tn);
+    u = nan(m,tn-1);
+    xDot = nan(n,tn-1);
+    x(:,1) = x0;
+    
+    for k = 1:tn-1
+        u(:,k) = g(x(:,k),t(k),k);
+        xDot(:,k) = f(x(:,k),u(:,k),t(k),k);
+        ts = t(k+1) - t(k);
+        x(:,k+1) = constrain(x(:,k) + xDot(:,k)*ts,xm,xM);
+    end
+    
+else % Simulate backward
+% If you simulate forward then backwards the answers will not be the same
+% because the u is based on left-side state in forward simulation and
+% right-side state in backward simulation. There might be way to make them
+% equal by formately picking the u at each step the backward simulation as
+% the one such that in one step forward simuation will result in the
+% right-hand state. Probably the same with xDot.
+
+    x = nan(n,tn);
+    u = nan(m,tn-1);
+    xDot = nan(n,tn-1);
+    x(:,end) = x0;
+    
+    for k = tn:-1:2
+        u(:,k-1) = g(x(:,k),t(k),k);
+        xDot(:,k-1) = f(x(:,k),u(:,k-1),t(k),k);
+        ts = t(k) - t(k-1);
+        x(:,k-1) = constrain(x(:,k) - xDot(:,k-1)*ts,xm,xM);
+    end
+
 end
 
 end
 
+%% Helper functions
 function x = constrain(x,xm,xM)
 x = min(max(x,xm),xM);
 end
